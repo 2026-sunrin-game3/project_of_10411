93b825d5ef3762a33dcd67bd8b379f9ae7429ac9
ojuns1013 <s26072@sunrint.hs.kr>
2026-07-09 17:43:07 +0900

    player movement

---
+++ Assets/script/player/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PlayerInput input;
+    private PlayerMovement movement;
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        movement = GetComponent<PlayerMovement>();
+    }
+    void Update()
+    {
+        movement.Move(input.axis);
+    }
+}
